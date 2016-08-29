b73e6db5bb5513923a00b6fce62fe3da6c5cd565
Christopher Welborn <cj@welbornprod.com>
2016-08-28 21:10:34 -0500

    Continuation, alignment, stderr by default.

--- printdebug/__init__.py
+++ printdebug/__init__.py
@@ -18,7 +18,7 @@ try:
 except ImportError:
     C = None
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 __all__ = [
     'DebugPrinter',
@@ -106,19 +106,25 @@ def debug(*args, **kwargs):
         Keyword Arguments:
             same as print()
             ..also:
-                parent       : Parent class to include name for methods.
-                level        : Number of frames to go back.
-                               Default: 1
+                align        : Omit the line info but use it's width as
+                               indention.
                 back         : Alias for `level`, will be deprecated soon.
-                fmt          : .format() string for line info.
-                               Default: '{filename}:{lineno} {name}(): '
-                ljustwidth   : str.ljust() value for line info.
-                               Default: 40
                 basename     : Whether to use just the base name of the file.
                                Default: False
+                fmt          : .format() string for line info.
+                               Default: printdebug.default_format
+                level        : Number of frames to go back.
+                               Default: 1
+                ljustwidth   : str.ljust() value for line info.
+                               Default: 40
+                parent       : Parent class to include name for methods.
     """
     if not (_enabled and args):
         return None
+
+    # Use stderr by default.
+    if kwargs.get('file', None) is None:
+        kwargs['file'] = sys.stderr
 
     # Include parent class name when given.
     parent = pop_or(kwargs, 'parent')
@@ -132,6 +138,9 @@ def debug(*args, **kwargs):
     # Get ljust level.
     ljustwidth = pop_or(kwargs, 'ljustwidth', 40)
 
+    # Are we omitting the line info, and just aligning with the end of it?
+    align = pop_or(kwargs, 'align', False)
+
     info = get_lineinfo(level=backlevel)
     usebasename = pop_or(kwargs, 'basename', True)
     fname = os.path.split(info.filename)[-1] if usebasename else info.filename
@@ -143,13 +152,30 @@ def debug(*args, **kwargs):
 
     # Patch args to stay compatible with print().
     pargs = list(args)
-    infostr = fmt.format(
+    lineinfo = fmt.format(
         filename=fname,
         lineno=info.lineno,
         name=func).ljust(ljustwidth)
+
+    # Is this a continuation from a previous line?
+    # Getting this for debug(), re-setting for print().
+    kwargs['end'] = kwargs.get('end', '\n')
+    willcontinue = (not kwargs['end'].endswith('\n'))
+    continued = debug.continued.get(kwargs['file'], False)
+    if align or continued:
+        debug.continued[kwargs['file']] = willcontinue
+        if align:
+            pargs[0] = ''.join((' ' * len(lineinfo), pargs[0]))
+        print(*pargs, **kwargs)
+        return None
+    debug.continued[kwargs['file']] = willcontinue
+
     text = kwargs.get('sep', ' ').join((str(s) for s in pargs))
-    line = ''.join((str(infostr), text))
+    line = ''.join((str(lineinfo), text))
     print(line, **kwargs)
+# This dict keeps track of whether a line is "continued", based on the last
+# `end` parameter, and it does so for each file descriptor used.
+debug.continued = {sys.stderr: False}
 
 
 def pop_or(dct, key, default=None):
@@ -218,15 +244,23 @@ class DebugPrinter(object):
         and uses it until changed.
     """
 
-    def __init__(self, fmt=None, ljustwidth=40, basename=True):
+    def __init__(self, fmt=None, ljustwidth=40, basename=True, file=None):
         self.fmt = fmt or default_format
         self.ljustwidth = ljustwidth
         self.basename = basename
+        # Use stderr by default.
+        self.file = file or sys.stderr
+        # Keeps track of line continuations, per file descriptor.
+        self.continued = {self.file: False}
 
     def debug(self, *args, **kwargs):
         """ Wrapper for print() that adds file, line, and func info. """
         if not (_enabled and args):
             return None
+        # Use stderr by default.
+        if kwargs.get('file', None) is None:
+            kwargs['file'] = self.file
+
         # Include parent class name when given.
         parent = kwargs.get('parent', None)
 
@@ -246,7 +280,7 @@ class DebugPrinter(object):
 
         # Patch args to stay compatible with print().
         pargs = list(args)
-        infostr = self.fmt.format(
+        lineinfo = self.fmt.format(
             filename=fname,
             lineno=info.lineno,
             name=func).ljust(self.ljustwidth)
@@ -254,8 +288,22 @@ class DebugPrinter(object):
             kwargs.get('sep', ' ').join((str(s) for s in pargs))
         ))
 
-        # infostr may be a Colr instance.
-        line = ''.join((str(infostr), text))
+        align = pop_or(kwargs, 'align', False)
+        # Is this a continuation from a previous line?
+        # Getting this for debug(), re-setting for print().
+        kwargs['end'] = kwargs.get('end', '\n')
+        willcontinue = (not kwargs['end'].endswith('\n'))
+        continued = self.continued.get(kwargs['file'], False)
+        if align or continued:
+            self.continued[kwargs['file']] = willcontinue
+            if align:
+                text = ''.join((' ' * self.lineinfo_len(lineinfo), text))
+            print(text, **kwargs)
+            return None
+        self.continued[kwargs['file']] = willcontinue
+
+        # lineinfo may be a Colr instance.
+        line = ''.join((str(lineinfo), text))
 
         # Pop all kwargs for this function. Send the rest to print().
         with suppress(KeyError):
@@ -266,6 +314,12 @@ class DebugPrinter(object):
             kwargs.pop('parent')
 
         print(line, **kwargs)
+
+    def lineinfo_len(self, s):
+        """ Overridable, returns the length of line info.
+            This is needed in subclasses because of escape codes.
+        """
+        return len(s)
 
     def transform_text(self, text):
         """ Run a transformation on the actual text before printing. """
@@ -293,6 +347,12 @@ class DebugColrPrinter(DebugPrinter):
             ljustwidth=ljustwidth,
             basename=basename,
         )
+
+    def lineinfo_len(self, s):
+        """ Return a line length, without escape codes. """
+        if hasattr(s, 'stripped'):
+            return len(s.stripped())
+        return len(s)
 
     def transform_text(self, text):
         """ Transform all debug text, colorizing it. """
--- printdebug/__main__.py
+++ printdebug/__main__.py
@@ -5,12 +5,12 @@
 """
 import sys
 import printdebug as pd
-from printdebug import get_lineinfo, printdebug
+from printdebug import __version__, get_lineinfo, debug
 
 
 def main():
     pd.debug('Hello from main().')
-    printdebug('The old functions still work.')
+    debug('The old functions still work.')
     return run_tests(
         another_func_test,
         debugprinter_test,
@@ -18,7 +18,26 @@ def main():
         lineinfo_str_test,
         color_format_test,
         level_test,
+        continued_test,
+        align_test,
     )
+
+
+def align_test():
+    """ debug(align=True) should align content without the lineinfo. """
+    debug('Next line will be aligned with this one.')
+    debug('...so is it?', align=True)
+    dp = pd.DebugPrinter()
+    dp.debug('DebugPrinter, next line will be aligned.')
+    dp.debug('...is it aligned?', align=True)
+
+    try:
+        cp = pd.DebugColrPrinter()
+    except ImportError:
+        return 0
+    cp.debug('DebugColrPrinter, next line will be aligned.')
+    cp.debug('...was it aligned?', align=True)
+    return 0
 
 
 def another_func_test():
@@ -52,6 +71,32 @@ def color_format_test():
     return 0
 
 
+def continued_test():
+    """ Check auto-line-continuation. """
+    # Intermingling these on purpose.
+    debug('Starting on stderr', end='')
+    debug('Starting on stdout', end='', file=sys.stdout)
+    debug(', and continuing.')
+    debug(', and continuing on stdout.', file=sys.stdout)
+    debug('A separate stderr line.')
+    debug('A separate stdout line.', file=sys.stdout)
+    dp = pd.DebugPrinter()
+    dp.debug('DebugPrinter, starting on stderr', end='')
+    dp.debug('DebugPrinter, starting on stdout', end='', file=sys.stdout)
+    dp.debug(', and continuing.')
+    dp.debug(', and continuing.', file=sys.stdout)
+    try:
+        cp = pd.DebugColrPrinter()
+    except ImportError:
+        return 0
+    cp.debug('DebugColrPrinter, starting on stderr', end='')
+    cp.debug('DebugColrrinter, starting on stdout', end='', file=sys.stdout)
+    cp.debug(', and continuing.')
+    cp.debug(', and continuing.', file=sys.stdout)
+
+    return 0
+
+
 def debugprinter_test():
     """ Debug printer uses it's config to format the line info. """
     dp = pd.DebugPrinter(
@@ -79,15 +124,22 @@ def disable_test():
 
 def level_test():
     """ Walk backwards through the frame levels. """
+    try:
+        dp = pd.DebugColrPrinter()
+    except ImportError as ex:
+        # If this is because colr is not installed, ignore it.
+        if ex.name != 'colr':
+            raise
+        dp = pd.DebugPrinter()
+
     def sub_function():
         def subsub_function():
-            cp = pd.DebugColrPrinter()
             for i in range(0, 10):
                 try:
                     info = get_lineinfo(level=i)
                 except ValueError:
                     break
-                cp.debug(''.join((' ' * (10 - i), info.name)))
+                dp.debug(''.join((' ' * (10 - i), info.name)))
         subsub_function()
     sub_function()
     return 0
@@ -118,4 +170,8 @@ def run_tests(*funcs):
 
 
 if __name__ == '__main__':
+    if ('-v' in sys.argv) or ('--version' in sys.argv):
+        print('printdebug v. {}'.format(__version__))
+        sys.exit(0)
+
     sys.exit(main())
