2f144b2e41f5fb7abebe202a33ae210ef185738d
Christopher Welborn <cj@welbornprod.com>
2016-08-28 21:12:29 -0500

    About to release pypi package 0.2.1.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ else:
 
 setup(
     name='PrintDebug',
-    version='0.2.0',
+    version='0.2.1',
     author='Christopher Welborn',
     author_email='cj@welbornprod.com',
     packages=['printdebug'],
